926b48f7fb9f3680a0f63e2151302c6b74c9156e
Ihor Yakushev <ihor.yakushev33@mail.com>
2025-03-16 10:53:02 +0200

    Task 2 is solved. HW9

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+        truth();
     }
+
+    public static void truth() {
+        System.out.println("Input your number: ");
+        int input = new Scanner(System.in).nextInt();
+        System.out.println(input % 2 == 0);
+    }
+
 }
