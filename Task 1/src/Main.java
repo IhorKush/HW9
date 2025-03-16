df2420ef372f4370c1b432284e697c023ff853b2
Ihor Yakushev <ihor.yakushev33@mail.com>
2025-03-16 10:23:31 +0200

    First task is solved. HW9

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,36 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     static double a = 0;
     static double b = 0;
 
     public static void main(String[] args) {
-        //todo Вам нужно дополнить методы так, чтобы получился рабочий калькулятор
+
         a = inputDouble();
         b = inputDouble();
 
-        sum();      //Метод суммирования чисел
-        multiply(); //Метод умножения чисел
-        diff();     //Метод вычитания чисел
-        divide();   //Метод деления чисел
+        sum();
+        multiply();
+        diff();
+        divide();
     }
 
     private static void divide() {
-        //todo дописать логику и печать результата
+        System.out.println(a + " / " + b + " = " + (a / b));
     }
 
     private static void diff() {
-        //todo дописать логику и печать результата
+        System.out.println(a + " - " + b + " = " + (a - b));
     }
 
     private static void multiply() {
-        //todo дописать логику и печать результата
+        System.out.println(a + " * " + b + " = " + (a * b));
     }
 
     private static void sum() {
-        //todo дописать логику и печать результата
+        System.out.println(a + " + " + b + " = " + (a + b));
     }
 
     private static double inputDouble() {
-        //todo дописать логику
-        return 0;
+        System.out.println("Input the number: ");
+        return new Scanner(System.in).nextDouble();
     }
 }
